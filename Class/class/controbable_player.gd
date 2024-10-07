87c905cfeb8910ce977ab9c23132ad6ce355a94e
Norika08 <nkatsuya@sva.edu>
2024-10-07 18:02:22 -0400

    class

--- Class/class/controbable_player.gd
+++ Class/class/controbable_player.gd
@@ -12,15 +12,15 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		print("I pressed a button")
 		
-		if Input.is_action_pressed("ui_left"):
-			position.x -=1
-			
-		if Input.is_action_pressed("ui_right"):
-			position.x -=1
-			
-		if Input.is_action_pressed("ui_up"):
-			position.x -=1
-			
-		if Input.is_action_pressed("ui_down"):
-			position.x -=1
-			
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed
+		
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed
+		
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed
+		
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed
+		
